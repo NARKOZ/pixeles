9984d6efb2f438f17e506dbef5fcbaacaa920987
Nihad Abbasov <nihad@42na.in>
2017-10-05 00:02:43 +0400

    install jquery

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -2,6 +2,7 @@ var path = require('path')
 var utils = require('./utils')
 var config = require('../config')
 var vueLoaderConfig = require('./vue-loader.conf')
+var webpack = require('webpack')
 
 function resolve (dir) {
   return path.join(__dirname, '..', dir)
@@ -25,6 +26,13 @@ module.exports = {
       '@': resolve('src'),
     }
   },
+  plugins: [
+    new webpack.ProvidePlugin({
+      $: 'jquery',
+      jQuery: 'jquery',
+      jquery: 'jquery'
+    })
+  ],
   module: {
     rules: [
       {
